7e394b30a3f09dada87df267def895a4b7f1171a
pratikpatelx <patelpk3@myumanitoba.ca>
2021-05-13 11:32:21 +0300

    Added React Particles Animation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import React from "react";
 import "./App.css";
+import ParticleAnimations from "./animations/ParticleAnimations";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <ParticleAnimations></ParticleAnimations>
+      <div className="home-wrapper">
+        <h1>Hello Test</h1>
+      </div>
+    </>
+  );
 }
 
 export default App;
