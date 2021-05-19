70f318021a50b1789b624252e7e850cd0923b38f
pratikpatelx <patelpk3@myumanitoba.ca>
2021-05-19 22:33:18 +0300

    Footer Implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,70 @@
 import React from "react";
+import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { GrMail } from "react-icons/gr";
 
 function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
   return (
-    <footer class="footer mt-auto py-3 bg-light">
-      <div className="container">
-        <span>Designed and Developed By Pratik Patel</span>
+    <div className="container-fluid footer">
+      <div className="row">
+        <div className="col footer-copyright">
+          <h3>Designed and Developed by Pratik Patel</h3>
+        </div>
+        <div className="col footer-copyright">
+          <h3>Copyright © {year} Pratik Patel</h3>
+        </div>
+        <div className="col footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/pratik-patel-622183b1/"
+                rel="noreferrer"
+                target="_blank"
+                aria_label="LinkedIn"
+                style={{ color: "white" }}
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://github.com/pratikpatelx"
+                rel="noreferrer"
+                target="_blank"
+                aria_label="Github"
+                style={{ color: "white" }}
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="mailto: patelpk3@myumanitoba.ca"
+                rel="noreferrer"
+                target="_blank"
+                aria_label="GMail"
+                style={{ color: "white" }}
+              >
+                <GrMail></GrMail>
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://twitter.com/pratikP123"
+                rel="noreferrer"
+                target="_blank"
+                aria_label="Twitter"
+                style={{ color: "white" }}
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
-    </footer>
+    </div>
   );
 }
 
