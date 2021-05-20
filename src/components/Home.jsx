7fb9f8ae31bd84563e3d520c2f0dc3c038ab5d05
pratikpatelx <patelpk3@myumanitoba.ca>
2021-05-21 01:29:37 +0300

    Changed Responsiveness

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,13 @@ import Hello from "../assets/Coding.svg";
 import "../App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import TypingAnimation from "../animations/TypingAnimation";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { GrMail } from "react-icons/gr";
 
 function Home() {
   return (
@@ -23,6 +30,55 @@ function Home() {
                 <strong className="name-color">
                   <TypingAnimation></TypingAnimation>
                 </strong>{" "}
+              </div>
+              <div className="icons-div">
+                <h1 className="h1-heading">Let's Connect</h1>
+                <p>
+                  Feel free to{" "}
+                  <span className="name-color"> get in touch with me</span>
+                </p>
+                <ul className="home-about-social-links">
+                  <li className="social-icons">
+                    <a
+                      href="https://github.com/pratikpatelx"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <AiFillGithub />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://www.linkedin.com/in/pratik-patel-622183b1/"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <FaLinkedinIn />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="https://twitter.com/pratikP123"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <AiOutlineTwitter />
+                    </a>
+                  </li>
+                  <li className="social-icons">
+                    <a
+                      href="mailto: patelpk3@myumanitoba.ca"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="icon-colour  home-social-icons"
+                    >
+                      <GrMail />
+                    </a>
+                  </li>
+                </ul>
               </div>
             </div>
             <div className="col-sm-6" id="col-test">
